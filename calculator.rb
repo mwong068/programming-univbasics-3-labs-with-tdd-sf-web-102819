a980754b7d47dace35ae72d1dca8cd219322e53c
Megan Wong <mwong068@gmail.com>
2019-10-16 23:37:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,16 @@
 first_number = 1
 second_number = 2 
+
 def addition(num1, num2)
  return num1 + num2
 end
 sum = addition(first_number, second_number)
+
+def subtraction(num1, num2)
+  return num1 - num2
+end
+difference = subtraction(first_number, second_number)
+
 
  
 
