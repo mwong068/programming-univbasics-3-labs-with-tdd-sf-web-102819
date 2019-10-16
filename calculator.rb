daf65235d943a0f34f71e818db7d5422f11a32bc
Megan Wong <mwong068@gmail.com>
2019-10-16 23:39:35 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,25 @@
-# Add your variables here
+first_number = 1
+second_number = 2 
+
+def addition(num1, num2)
+ return num1 + num2
+end
+sum = addition(first_number, second_number)
+
+def subtraction(num1, num2)
+  return num1 - num2
+end
+difference = subtraction(first_number, second_number)
+
+def multiply(num1, num2)
+  return num1 * num2
+end
+product = multiply(first_number, second_number)
+
+def divide(num1, num2)
+  return num1 / num2
+end
+quotient = divide(first_number, second_number)
+ 
+
+
