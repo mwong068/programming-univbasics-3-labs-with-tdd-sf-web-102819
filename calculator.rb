8d2b8bc77dbce993f86d45a8d9122a574fafeab7
Megan Wong <mwong068@gmail.com>
2019-10-16 23:32:54 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,14 @@
-# Add your variables here
+first_number = 1
+second_number = 2 
+def calculator  
+ sum = first_number + second_number
+   return sum
+ difference = first_number - second_number
+   return difference
+product = first_number * second_number
+   return product
+ quotient = first_number / second_number
+   return quotient
+end
+
+
