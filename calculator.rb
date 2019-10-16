38e9c6e244760e71f758cba10357736218c962f3
Megan Wong <mwong068@gmail.com>
2019-10-16 23:33:35 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,8 @@
 first_number = 1
 second_number = 2 
 def sum(num1, num2)
- sum = first_number + second_number
-   return sum
+ return first_number + second_number
+end
  difference = first_number - second_number
    return difference
 product = first_number * second_number
